0d3e39a3a87e755071f5ef0b0a1b56ba5b5dc092
Eduardo Mucelli Rezende Oliveira <eduardo.mucelli@blablacar.com>
2017-07-26 19:56:03 +0200

    * Revampl plot to use the output from the java predictor
    Former-commit-id: 0c4f40f5b7d91c531f20817b46510dc4b9d587ec

--- plot/plots.R
+++ plot/plots.R
@@ -1,17 +1,42 @@
-library(reshape2)
 library(ggplot2)
+library(reshape)
+library(scales)
+library(dplyr)
+library(tidyr)
 
-data = read.csv('~/Desktop/benchmark_ml.tsv', sep='\t', stringsAsFactor=FALSE)
-data = melt(data, id.vars = c("Package"), measure.vars= c(paste0("UC",1:9) ))
+theme_paper <- function() {
+  theme_update( panel.background  = element_blank(),
+                panel.grid.major  = element_blank(),
+                axis.title.x      = element_text(vjust = 0.5),
+                axis.title.y      = element_text(angle = 90, vjust = 1),
+                axis.text.x       = element_text(angle = 45, hjust=1),  
+                plot.margin       = unit(c(1, 1, 1, 1), "lines"),
+                plot.title        = element_text(face="bold", hjust = 0.5))
+}
 
-data$mean = as.numeric(lapply( strsplit(data$value, split=" "), function(x) x[1]))
-data$se = as.numeric(lapply( strsplit(data$value, split=" "), function(x) as.numeric(x[3])))
+theme_set(theme_bw())
+theme_paper()
 
-data$variable = factor(data$variable, levels=paste0("UC",c(1,4,7,2,5,8,3,6,9)))
+method = "boosting" # boosting
+input_file = paste0("results_", method, ".csv")
+output_file = paste0("benchmark_", method, ".pdf")
 
-ggplot(data, aes(x=Package, y=mean, color=Package, fill=Package)) + 
-  geom_bar(stat="identity" ) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1) +
-  facet_wrap(~variable) + 
-  ggtitle('Benchmark ML using Boosting Machine')+ 
-  ylab('Mean prediction time (ms)')
+data = read.csv(input_file, col.names = c('ROWS', 'COLUMNS', 'TIME_MILLIS', 'MODEL'), header = FALSE)
+data %>% unite(UC, ROWS, COLUMNS, sep = "x") %>%
+         mutate(UC = factor(UC, levels=c("100x100", "500x100", "1000x100", "100x500", "500x500", "1000x500", "100x1000", "500x1000", "1000x1000"), 
+                                labels=c("UC 1 (100x100)", "UC 2 (500x100)", "UC 3 (1000x100)", "UC 4 (100x500)", "UC 5 (500x500)", "UC 6 (1000x500)", "UC 7 (100x1000)", "UC 8 (500x1000)", "UC 9 (1000x1000)"))) %>%
+         group_by(UC, MODEL) %>%
+         summarize(MEAN_TIME_MILLIS = mean(TIME_MILLIS),
+                   SE_TIME_MILLIS = 1.96 * sd(TIME_MILLIS) / sqrt(n())) %>%
+         arrange(MODEL) %>%
+    ggplot(., aes(x = as.factor(MODEL), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+        labs(title = "XGBoost Java") +
+        xlab("Number of columns") +
+        ylab("Time (ms)") +
+        ggtitle('Benchmark ML using GLM and XGBoost linear') + 
+        geom_bar(stat="identity") +
+        geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
+        # scale_y_continuous(breaks = seq(0, 150, 15)) +
+        facet_wrap(~UC) +
+        theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank(), axis.text.x = element_blank())
+ggsave(output_file)
