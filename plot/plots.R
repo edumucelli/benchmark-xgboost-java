c3d3d9e530de9e8708766f6a94199094bc05f624
Eduardo Mucelli Rezende Oliveira <eduardo.mucelli@blablacar.com>
2017-09-25 18:27:30 +0200

    Update plot and results
    Former-commit-id: a71b618e82b86040f27c32b42880f3fcbdfe04e1

--- plot/plots.R
+++ plot/plots.R
@@ -17,25 +17,120 @@ theme_paper <- function() {
 theme_set(theme_bw())
 theme_paper()
 
-method = "boosting" # boosting
-input_file = paste0("results_", method, ".csv")
-output_file = paste0("benchmark_", method, ".pdf")
+options("width"=200)
 
-data = read.csv(input_file, col.names = c('ROWS', 'COLUMNS', 'TIME_MILLIS', 'MODEL'), header = FALSE)
-data %>% unite(UC, ROWS, COLUMNS, sep = "x") %>%
-         mutate(UC = factor(UC, levels=c("100x100", "500x100", "1000x100", "100x500", "500x500", "1000x500", "100x1000", "500x1000", "1000x1000"), 
-                                labels=c("UC 1 (100x100)", "UC 2 (500x100)", "UC 3 (1000x100)", "UC 4 (100x500)", "UC 5 (500x500)", "UC 6 (1000x500)", "UC 7 (100x1000)", "UC 8 (500x1000)", "UC 9 (1000x1000)"))) %>%
-         group_by(UC, MODEL) %>%
-         summarize(MEAN_TIME_MILLIS = mean(TIME_MILLIS),
-                   SE_TIME_MILLIS = 1.96 * sd(TIME_MILLIS) / sqrt(n())) %>%
-         arrange(MODEL) %>%
-    ggplot(., aes(x = as.factor(MODEL), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
-        labs(title = "XGBoost Java") +
-        xlab("Number of columns") +
+
+
+plot_linear_charts <- function (data) {
+
+    title = 'Benchmark ML using GLM and XGBoost Linear'
+
+    grouped_by_uc_model = data %>% unite(UC, ROWS, COLUMNS, sep = "x") %>%
+                                   mutate(UC = factor(UC, levels=c("100x100", "500x100", "1000x100", "100x500", "500x500", "1000x500", "100x1000", "500x1000", "1000x1000"), 
+                                                          labels=c("UC 1 (100x100)", "UC 2 (500x100)", "UC 3 (1000x100)", "UC 4 (100x500)", "UC 5 (500x500)", "UC 6 (1000x500)", "UC 7 (100x1000)", "UC 8 (500x1000)", "UC 9 (1000x1000)"))) %>%
+                                   group_by(UC, MODEL) %>%
+                                   summarize(MEAN_TIME_MILLIS = mean(TIME_MILLIS),
+                                             SE_TIME_MILLIS = 1.96 * sd(TIME_MILLIS) / sqrt(n())) %>%
+                                   arrange(MODEL)
+
+    ggplot(grouped_by_uc_model, aes(x = as.factor(MODEL), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+        ggtitle(title) + 
         ylab("Time (ms)") +
-        ggtitle('Benchmark ML using Boosting') + 
         geom_bar(stat="identity") +
         geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
         facet_wrap(~UC) +
         theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank(), axis.text.x = element_blank())
-ggsave(output_file)
+    ggsave("linear_time_by_model_facet_uc.pdf")
+
+    ggplot(grouped_by_uc_model, aes(x = as.factor(UC), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+        ggtitle(title) + 
+        ylab("Time (ms)") +
+        geom_bar(stat="identity") +
+        geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
+        facet_wrap(~MODEL) +
+        theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank())
+    ggsave("linear_time_by_uc_facet_model.pdf")
+
+    grouped_by_uc_model %>%
+    filter(UC == "UC 9 (1000x1000)", MODEL %in% c(" XGB-Predictor (xgbLinear)", " XGBoost4J (xgbLinear)")) %>%
+    ggplot(., aes(x = as.factor(MODEL), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+        ylab("Time (ms)") +
+        ggtitle(title) + 
+        geom_bar(stat="identity") +
+        geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
+        theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank(), axis.text.x = element_blank())
+    ggsave("linear_zoom_uc9_linear.pdf")
+
+    grouped_by_uc_model %>%
+    filter(UC %in% c("UC 1 (100x100)", "UC 2 (500x100)", "UC 3 (1000x100)")) %>%
+    ggplot(., aes(x = as.factor(MODEL), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+        ylab("Time (ms)") +
+        ggtitle(title) + 
+        geom_bar(stat="identity") +
+        geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
+        theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank(), axis.text.x = element_blank()) +
+        facet_wrap(~UC)
+    ggsave("linear_zoom_uc123_linear.pdf")
+
+}
+
+plot_boosting_charts <- function (data) {
+
+    title = 'Benchmark ML using XGBoost'
+
+    grouped_by_uc_model = data %>% unite(UC, ROWS, COLUMNS, sep = "x") %>%
+                                   mutate(UC = factor(UC, levels=c("100x100", "500x100", "1000x100", "100x500", "500x500", "1000x500", "100x1000", "500x1000", "1000x1000"), 
+                                                          labels=c("UC 1 (100x100)", "UC 2 (500x100)", "UC 3 (1000x100)", "UC 4 (100x500)", "UC 5 (500x500)", "UC 6 (1000x500)", "UC 7 (100x1000)", "UC 8 (500x1000)", "UC 9 (1000x1000)"))) %>%
+                                   group_by(UC, MODEL) %>%
+                                   summarize(MEAN_TIME_MILLIS = mean(TIME_MILLIS),
+                                             SE_TIME_MILLIS = 1.96 * sd(TIME_MILLIS) / sqrt(n())) %>%
+                                   arrange(MODEL)
+
+    # grouped_by_uc_model %>% filter(MODEL %in% c(" XGB-Predictor (xgbTree)"))
+
+    ggplot(grouped_by_uc_model, aes(x = as.factor(MODEL), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+        ggtitle(title) + 
+        ylab("Time (ms)") +
+        geom_bar(stat="identity") +
+        geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
+        facet_wrap(~UC) +
+        theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank(), axis.text.x = element_blank())
+    ggsave("boosting_time_by_model_facet_uc.pdf")
+
+    ggplot(grouped_by_uc_model, aes(x = as.factor(UC), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+        ggtitle(title) + 
+        ylab("Time (ms)") +
+        geom_bar(stat="identity") +
+        geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
+        facet_wrap(~MODEL) +
+        theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank())
+    ggsave("boosting_time_by_uc_facet_model.pdf")
+
+    grouped_by_uc_model %>%
+    filter(UC == "UC 9 (1000x1000)", MODEL %in% c(" RJava (GBM)", " XGBoost4J (xgbTree)")) %>%
+    ggplot(., aes(x = as.factor(MODEL), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+        ylab("Time (ms)") +
+        ggtitle(title) + 
+        geom_bar(stat="identity") +
+        geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
+        theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank(), axis.text.x = element_blank())
+    ggsave("boosting_zoom_uc9_boosting.pdf")
+
+    # grouped_by_uc_model %>%
+    # filter(UC %in% c("UC 1 (100x100)", "UC 2 (500x100)", "UC 3 (1000x100)")) %>%
+    # ggplot(., aes(x = as.factor(MODEL), y = MEAN_TIME_MILLIS, color = MODEL, fill = MODEL)) +
+    #     ylab("Time (ms)") +
+    #     ggtitle(title) + 
+    #     geom_bar(stat="identity") +
+    #     geom_errorbar(aes(ymin = MEAN_TIME_MILLIS-SE_TIME_MILLIS, ymax = MEAN_TIME_MILLIS+SE_TIME_MILLIS), width = 0.2) +
+    #     theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank(), axis.text.x = element_blank()) +
+    #     facet_wrap(~UC)
+    # ggsave("boosting_zoom_uc123_linear.pdf")
+
+}
+
+linear_data = read.csv("results_linear.csv", col.names = c('ROWS', 'COLUMNS', 'TIME_MILLIS', 'MODEL'), header = FALSE)
+plot_linear_charts(linear_data)
+
+boosting_data = read.csv("results_boosting.csv", col.names = c('ROWS', 'COLUMNS', 'TIME_MILLIS', 'MODEL'), header = FALSE)
+plot_boosting_charts(boosting_data)
---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by eduardo on 7/31/17.
+ */
+public class Test {
+}
